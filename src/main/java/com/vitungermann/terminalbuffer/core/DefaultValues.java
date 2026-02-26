e4ede0a56651b75007c83844e40f99a69cdbda10
VitekU <ungermann.vit@gmail.com>
2026-02-26 22:36:12 +0100

    feat: add class for constants used as default values

---
+++ src/main/java/com/vitungermann/terminalbuffer/core/DefaultValues.java
@@ -0,0 +1,11 @@
+package com.vitungermann.terminalbuffer.core;
+
+import com.vitungermann.terminalbuffer.helper.Style;
+import com.vitungermann.terminalbuffer.helper.TerminalColor;
+
+public final class DefaultValues {
+    public static final char defaultChar = ' ';
+    public static final Style defaultStyle = Style.NORMAL;
+    public static final TerminalColor defaultForeground = TerminalColor.BLACK;
+    public static final TerminalColor defaultBackground = TerminalColor.WHITE;
+}
