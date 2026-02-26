bae58b2e530860345c5949d06bf92ab1b5abcc18
VitekU <ungermann.vit@gmail.com>
2026-02-26 18:22:53 +0100

    feat: add helper classes CursorPosition and enum Terminal color

---
+++ src/main/java/com/vitungermann/terminalbuffer/helper/CursorPosition.java
@@ -0,0 +1,11 @@
+package com.vitungermann.terminalbuffer.helper;
+
+public class CursorPosition {
+    public int row;
+    public int column;
+
+    public CursorPosition(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+}
