5ba8252882b1659bc56b2bc892984ba006a89f9c
VitekU <ungermann.vit@gmail.com>
2026-02-27 21:27:19 +0100

    refactor: completely refactor the screen part of the buffer, move to a separate class and rewrite it

---
+++ src/main/java/com/vitungermann/terminalbuffer/core/Screen.java
@@ -0,0 +1,73 @@
+package com.vitungermann.terminalbuffer.core;
+
+import com.vitungermann.terminalbuffer.helper.CursorPosition;
+import com.vitungermann.terminalbuffer.helper.Style;
+import com.vitungermann.terminalbuffer.helper.TerminalColor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Screen {
+    private List<List<CharacterCell>> rows;
+    private int height;
+    private int width;
+    private TerminalColor currentForeground;
+    private TerminalColor currentBackground;
+    private Style currentStyle;
+
+    public String getScreenToString() {
+        StringBuilder sb = new StringBuilder();
+        for (var row : rows) {
+            for (var cell : row) {
+                sb.append(cell.getChar());
+            }
+        }
+        return sb.toString();
+    }
+
+    public void write(String text) {
+        List<CharacterCell> newRow = new ArrayList<>();
+        var textArr = text.toCharArray();
+        for (char character : textArr) {
+
+            newRow.add(new CharacterCell(character, currentForeground, currentBackground, currentStyle));
+        }
+        this.rows.add(newRow);
+    }
+
+    public void insert(String text, CursorPosition startingPos) {
+        int blockIndex = 0;
+        int count = 0;
+        int posIndex = startingPos.row * width + startingPos.column;
+        while (count < posIndex) {
+            count += rows.get(blockIndex).size();
+            blockIndex++;
+        }
+        blockIndex--;
+        List<CharacterCell> splitRow = rows.get(blockIndex);
+        List<CharacterCell> firstPart = new ArrayList<>(splitRow.subList(0, startingPos.column));
+        List<CharacterCell> secondPart = new ArrayList<>(splitRow.subList(startingPos.column, splitRow.size()));
+
+        rows.set(blockIndex, firstPart);
+        rows.add(blockIndex + 1, secondPart);
+
+        List<CharacterCell> newRow = new ArrayList<>();
+        var textArr = text.toCharArray();
+        for (char character : textArr) {
+
+            newRow.add(new CharacterCell(character, currentForeground, currentBackground, currentStyle));
+        }
+        this.rows.add(blockIndex + 1, newRow);
+    }
+
+    public Screen(int height, int width, TerminalColor currentForeground, TerminalColor currentBackground, Style currentStyle)
+    {
+        this.height = height;
+        this.width = width;
+        this.currentForeground = currentForeground;
+        this.currentBackground = currentBackground;
+        this.currentStyle = currentStyle;
+
+        rows = new ArrayList<>();
+    }
+}
--- src/main/java/com/vitungermann/terminalbuffer/core/TerminalBuffer.java
+++ src/main/java/com/vitungermann/terminalbuffer/core/TerminalBuffer.java
@@ -5,6 +5,7 @@ import com.vitungermann.terminalbuffer.helper.Direction;
 import com.vitungermann.terminalbuffer.helper.Style;
 import com.vitungermann.terminalbuffer.helper.TerminalColor;
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.vitungermann.terminalbuffer.helper.MathHelp.clamp;
 
@@ -14,57 +15,25 @@ public class TerminalBuffer {
 
     private final int maxScrollback;
     private CursorPosition cursorPosition;
-    private ArrayList<CharacterCell[]> screen;
-    private Scrollback scrollback;
 
-    private TerminalColor currentForeground;
-    private TerminalColor currentBackground;
-    private Style currentStyle;
+    private Scrollback scrollback;
 
     public String getScrollback() {
         return scrollback.getScrollback();
-    }
-    public String getScreen() {
-        StringBuilder sb = new StringBuilder();
-        for (CharacterCell[] row : screen) {
-            for (CharacterCell cc : row) {
-               if (cc == null) {
-                   sb.append(" ");
-               }
-               else {
-                   sb.append(cc.getChar());
-               }
-            }
-            sb.append("\n");
-        }
-        return sb.toString();
     }
 
     public CursorPosition getCursorPosition() {
         return cursorPosition;
     }
 
-    public void addEmptyLine() {
-        CharacterCell[] row = new CharacterCell[width];
-        this.screen.add(row);
+    public void setCursorPosition(int row, int column) {
+        this.cursorPosition.row = row;
+        this.cursorPosition.column = column;
     }
 
-    public void write(String text) {
-        for (int i = 0; i < text.length(); i++) {
-            CharacterCell newChar = new CharacterCell(text.charAt(i), currentForeground, currentBackground, currentStyle);
-            this.screen.get(cursorPosition.row)[cursorPosition.column] = newChar;
-            int newLines = moveCursor(Direction.RIGHT, 1);
-            for (int j = 0; j < newLines; j++) {
-                this.scrollback.add(this.screen.removeFirst());
-                addEmptyLine();
-            }
-        }
-    }
-
-    public int moveCursor(Direction direction, int n) {
+    public void moveCursor(Direction direction, int n) {
         int newRow = cursorPosition.row;
         int newColumn = cursorPosition.column;
-        int linesToAdd = 0;
         if (direction == Direction.UP) {
             newRow -= n;
         }
@@ -84,12 +53,8 @@ public class TerminalBuffer {
             }
         }
 
-        if (newRow >= height) {
-            linesToAdd = newRow - height + 1;
-        }
         this.cursorPosition.row = clamp(newRow, height);
         this.cursorPosition.column = clamp(newColumn, width);
-        return linesToAdd;
     }
 
 
@@ -100,12 +65,6 @@ public class TerminalBuffer {
         cursorPosition = new CursorPosition(0,0);
 
         this.scrollback = new Scrollback(maxScrollback);
-        this.screen = new ArrayList<>();
-
-        for (int i = 0; i < height; i++) {
-            addEmptyLine();
-        }
-
     }
 
 }
